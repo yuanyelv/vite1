90a044500259a2209ef8f96b8740870a59d5003b
yuanyelv <287142032@qq.com>
2022-05-19 19:47:02 +0800

    uu

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: yuanyelv 287142032@qq.com
+ * @Date: 2022-05-19 14:57:05
+ * @LastEditors: yuanyelv 287142032@qq.com
+ * @LastEditTime: 2022-05-19 19:45:15
+ * @FilePath: \vite-project\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
@@ -5,7 +13,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: yuanyelv 287142032@qq.com
+ * @Date: 2022-05-19 14:57:05
+ * @LastEditors: yuanyelv 287142032@qq.com
+ * @LastEditTime: 2022-05-19 19:46:02
+ * @FilePath: \vite-project\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 
